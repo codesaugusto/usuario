e69f22d678d7d32e4fcbdcea7b8c90880ad338db
augustok99 <carlosaugustox6@gmail.com>
2026-03-24 13:14:01 -0400

    adicionadas classes de security

---
+++ src/main/java/com/malloc/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.malloc.usuario.infrastructure.repository;
+
+
+import com.malloc.usuario.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+}
